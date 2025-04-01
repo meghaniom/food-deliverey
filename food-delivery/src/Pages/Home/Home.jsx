488b94167015c71b2ecd7cb8aa79f45ebd34ca26
meghani om <meghanio264@gmail.com>
2025-04-01 18:11:50 +0530

    second chang

--- food-delivery/src/Pages/Home/Home.jsx
+++ food-delivery/src/Pages/Home/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Home.css'
+import Header from '../../Components/Header/Header';
+import ExploreMenu from '../../Components/Exploremenu/ExploreMenu';
+import FoodDisplay from '../../Components/FoodDisplay/FoodDisplay';
 
 const Home = () => {
+  const [category, setCategory]= useState("All");
   return (
-    <div>Home</div>
+    <div>
+      <Header/>
+    <ExploreMenu category={category} setCategory = {setCategory}/>
+    <FoodDisplay category= {category} setCategory= {setCategory}/>
+    </div>
   )
 }
 
