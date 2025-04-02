9339130da607680e0ce1c2908c0c048497dec745
meghani om <meghanio264@gmail.com>
2025-04-02 10:20:39 +0530

    package

--- food-delivery/src/App.jsx
+++ food-delivery/src/App.jsx
@@ -1,22 +1,25 @@
-import React from 'react'
-import Navbar from './Components/Navbar/Navbar'
-import { Route, Routes } from 'react-router-dom'
-import Home from './Pages/Home/Home'
-import Cart from './Pages/Cart/Cart'
-import Placeorder from './Pages/Placeorder/Placeorder'
-
+import React from "react";
+import Navbar from "./Components/Navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
+import Cart from "./Pages/Cart/Cart";
+import Placeorder from "./Pages/Placeorder/Placeorder";
+import Footer from "./Components/Footer/Footer";
 
 const App = () => {
   return (
-    <div className='app'>
-      <Navbar/> 
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/cart' element={<Cart/>} />
-        <Route path='/order' element={<Placeorder/>} />
-        </Routes>      
-    </div>
-  )
-}
+    <>
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<Placeorder />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
 
-export default App
+export default App;
